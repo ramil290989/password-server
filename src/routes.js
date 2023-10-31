cfce7c43934823155d50fa72b3bede1e86021058
ramil290989 <garifullinramil290989@gmail.com>
2023-10-31 23:30:31 +0500

    install dependencies dotenv and http-errors

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const routes = (app, state) => {
   app.get('/login', (request, response) => {
+    const { username, password } = request.body;
+    const user = state.users.find((u) => u.username === username);
     
   });
 }
