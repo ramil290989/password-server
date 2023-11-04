d771230bc63a6041d22dbb97c1da3324ba57f701
ramil290989 <garifullinramil290989@gmail.com>
2023-11-05 01:35:13 +0500

    fiz route '/login', add route '/signin'

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,36 @@
-import express from 'express';
+import express, { response } from 'express';
 import { HttpError } from 'http-errors';
 import { generateToken } from './token.js';
+import state from './state.js';
+import addNewUser from './addNewUser.js';
 
 const { Unauthorized, Conflict } = HttpError;
+const { users, passwords } = state;
 
 const router = express.Router();
 
 router.post('/login', (request, response) => {
-  const body = request.body;
-  console.log(body);
-  response.send('token');
+  const { username, password } = request.body;
+  const user = users.find((u) => u.name === username);
+  if (user && password === user.password) {
+    const token = generateToken(user.name);
+    response.send({ userId: user.id, token });
+  } else {
+    response.send(Unauthorized());
+  }
 });
+router.post('/signIn', (request, response) => {
+  const { username, password } = request.body;
+  const user = users.find((u) => u.name === username);
+  if (user) {
+    response.send(Conflict());
+  } else {
+    const id = users.length + 1;
+    const newUser = { id, name: username, password };
+    addNewUser(newUser);
+    const token = generateToken(username);
+    response.send({ userId: id, token });
+  }
+})
 
 export default router;
