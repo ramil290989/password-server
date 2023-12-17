719744def83822324b7c791bee1c87686b810e7c
ramil290989 <garifullinramil290989@gmail.com>
2023-12-18 02:58:40 +0500

    fix start

--- bin/start.js
+++ bin/start.js
@@ -1,16 +1,3 @@
-import express from 'express';
-import router from '../src/routes.js';
+import start from '../index.js';
 
-const PORT = process.env.port || 3005;
-
-const start = () => {
-  const app = express();
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use('/api', router);
-  app.listen(PORT, () => {
-    console.log(`Server works on port ${PORT}`);
-  });
-};
-
-export default start;
+start();
--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 #! /usr/bin/env node
-import start from './bin/start.js';
+import express from 'express';
+import router from './src/routes.js';
 
-start();
+const PORT = process.env.port || 3005;
+
+const start = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use('/api', router);
+  app.listen(PORT, () => {
+    console.log(`Server works on port ${PORT}`);
+  });
+};
+
+export default start;
